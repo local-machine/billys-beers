d4c3ff7e1459bcb424afdc0ff7893ac66ca134ad
Student <example@example.com>
2019-09-09 14:23:59 -0700

    It will have img with CSS styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import Header from './Header';
 import BeerList from './BeerList';
 import { Switch, Route } from 'react-router-dom';
 import NewBeerForm from './NewBeerForm';
+import billLaughs from '../assets/images/bill.jpg';
+
+
 
 function App() {
   return (
@@ -13,11 +16,38 @@ function App() {
             font-family: Helvetica;
           }
         `}</style>
+      <style jsx>{`
+          img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+          }
+          img:hover {
+            animation: shake 0.5s;
+            animation-iteration-count: infinite;
+          }
+          
+          @keyframes shake {
+            0% { transform: translate(1px, 1px) rotate(0deg); }
+            10% { transform: translate(-1px, -2px) rotate(-1deg); }
+            20% { transform: translate(-3px, 0px) rotate(1deg); }
+            30% { transform: translate(3px, 2px) rotate(0deg); }
+            40% { transform: translate(1px, -1px) rotate(1deg); }
+            50% { transform: translate(-1px, 2px) rotate(-1deg); }
+            60% { transform: translate(-3px, 1px) rotate(0deg); }
+            70% { transform: translate(3px, 1px) rotate(-1deg); }
+            80% { transform: translate(-1px, -1px) rotate(1deg); }
+            90% { transform: translate(1px, 2px) rotate(0deg); }
+            100% { transform: translate(1px, -2px) rotate(-1deg); }
+          }
+        `}</style>
       <Header />
       <Switch>
         <Route exact path='/' component={BeerList} />
-        <Route path='/newbeer' component={NewBeerForm}/>
+        <Route path='/newbeer' component={NewBeerForm} />
       </Switch>
+      <img src={billLaughs} />
     </div>
   );
 }
